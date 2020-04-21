2b0993230e94c9669582de95158a846b3be14be6
Nivall <38981440+k-kotov@users.noreply.github.com>
2020-04-21 11:44:10 +0300

    Update main.tf

--- tests/main.tf
+++ tests/main.tf
@@ -9,7 +9,3 @@ resource "aws_instance" "base-ubuntu1604-devel2-201906270507" {
   instance_type = "t2.nano"
 }
 
-resource "aws_instance" "base-ubuntu1604-devel2-201906270507" {
-  ami           = "ami-04c8b7095c907cd8b"
-  instance_type = "t2.nano"
-}
