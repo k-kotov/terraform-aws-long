efc660c864d1f21183cfdd73607950b9df26fe58
Nivall <38981440+k-kotov@users.noreply.github.com>
2021-05-06 15:40:58 +0300

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,35 +1,9 @@
 
-module "win" {
-   source  = "./azure"
-   }
- 
+
  module "win111" {
    source  = "./test"
    }
-
-    
-  module "qa" {
-    source  = "./google"
-                    name = "kostya"
-    } 
-   
-   module "xxx" {
-   source  = "./aws"
-   }
-      
-      module "aaa" {
-   source  = "./ec2"
-   }
  
-     
-               
- 
- #module "v1" {
- #source = "github.com/k-kotov/33.git"
-#}
-  
-
-    
    
 
     
